ac538da2405b864d3054e50e7d0e22e8e6617ead
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2026-03-17 02:19:08 +0000

    Update brainIntent.js

--- chatbot/brainIntent.js
+++ chatbot/brainIntent.js
@@ -1,29 +1,28 @@
 const net = new brain.NeuralNetwork();
 
 net.train([
-{ input: {paint:1}, output:{paintQuantity:1}},
-{ input: {room:1}, output:{paintQuantity:1}},
-{ input: {wall:1}, output:{paintQuantity:1}},
-
-{ input: {hello:1}, output:{greeting:1}},
-{ input: {hi:1}, output:{greeting:1}}
+  { input: {paint:1}, output:{paintQuantity:1}},
+  { input: {room:1}, output:{paintQuantity:1}},
+  { input: {wall:1}, output:{paintQuantity:1}},
+  { input: {hello:1}, output:{greeting:1}},
+  { input: {hi:1}, output:{greeting:1}}
 ]);
 
-function detectIntentBrain(message){
+export function detectIntentBrain(message){
 
-message = message.toLowerCase();
+  message = message.toLowerCase();
 
-const input = {
-paint: message.includes("paint") ? 1 : 0,
-room: message.includes("room") ? 1 : 0,
-wall: message.includes("wall") ? 1 : 0,
-hello: message.includes("hello") ? 1 : 0,
-hi: message.includes("hi") ? 1 : 0
-};
+  const input = {
+    paint: message.includes("paint") ? 1 : 0,
+    room: message.includes("room") ? 1 : 0,
+    wall: message.includes("wall") ? 1 : 0,
+    hello: message.includes("hello") ? 1 : 0,
+    hi: message.includes("hi") ? 1 : 0
+  };
 
-const result = net.run(input);
+  const result = net.run(input);
 
-let intent = Object.keys(result).reduce((a,b)=>result[a] > result[b] ? a : b);
+  let intent = Object.keys(result).reduce((a,b)=>result[a] > result[b] ? a : b);
 
-return intent;
+  return intent;
 }
