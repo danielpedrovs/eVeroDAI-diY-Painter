ebeb35f27b63d333480174764bbf670298376014
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2026-03-17 15:21:24 +0000

    Update handlers.js

--- chatbot/handlers.js
+++ chatbot/handlers.js
@@ -1,6 +1,6 @@
 import { responses } from "./responses.js";
 import { extractDimensions, extractRatePerSquareMeter } from "./parser.js";
-import { estimateLabourCost, formatMoney} from "./costLibrary.js";
+import { estimateLabourCost, formatMoney, getDefaultRatePerM2 } from "./costLibrary.js";
 import { problems } from "./problems.js";
 import { knowledge } from "./knowledge.js";
 
@@ -91,7 +91,7 @@ timeEstimate(){
 }
 
 let area = dims.width * dims.height;
-let rate = extractRatePerSquareMeter(message);
+let rate = extractRatePerSquareMeter(message) || getDefaultRatePerM2();
 let total = estimateLabourCost(area, rate);
 
 return `estimated labour cost:
