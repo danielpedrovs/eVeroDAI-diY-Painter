d5b6616166a1aa87b67ae2d5d11bf034869d3164
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2026-03-17 14:51:20 +0000

    Update costlibrary.js

--- chatbot/costlibrary.js
+++ chatbot/costlibrary.js
@@ -1,1 +1,15 @@
+export const DEFAULT_RATE_PER_M2 = 12;
 
+export function estimateLabourCost(area, ratePerM2 = DEFAULT_RATE_PER_M2){
+  const total = area * ratePerM2;
+  return Number(total.toFixed(2));
+}
+
+export function formatMoney(amount, currency = "GBP", locale = "en-GB"){
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(amount);
+}
