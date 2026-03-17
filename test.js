b8a2beb9296167f70a1983c462cdb6cb75600cb8
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2026-03-17 14:41:47 +0000

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,12 @@ const tests = [
     "how much paint 4 walls 5 by 3",
     "my wall has a crack",
     "the paint is peeling"
+      "hello",
+    "how much paint 4 walls 5 by 3",
+    "my wall has a crack",
+    "the paint is peeling"
+    "the paint is peeling",
+    "how long it will take"
 ];
 
 for(let msg of tests){
